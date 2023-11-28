f51a459f9e10edcfedc49dc3a90c7569e0f49491
YefferSime <simehuaranccay@gmail.com>
2023-11-28 13:42:41 -0500

    commit

--- Entorno/iTravel/iTravel/settings.py
+++ Entorno/iTravel/iTravel/settings.py
@@ -135,5 +135,5 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5174",
 ]
--- Entorno/iTravelFront/src/api/Empresas.api.js
+++ Entorno/iTravelFront/src/api/Empresas.api.js
@@ -2,3 +2,21 @@ import axios from 'axios'
 export const getAllEmpresas=()=>{
     return axios.get('http://127.0.0.1:9090/empresas/')
 }
+export const getAllUsuarios=()=>{
+    return axios.get('http://127.0.0.1:9090/usuarios/')
+}
+export const getAllBuses=()=>{
+    return axios.get('http://127.0.0.1:9090/buses/')
+}
+export const getAllBoletos=()=>{
+    return axios.get('http://127.0.0.1:9090/boletos/')
+}
+export const getAllChoferes=()=>{
+    return axios.get('http://127.0.0.1:9090/choferes/')
+}
+export const getAllItinerarios=()=>{
+    return axios.get('http://127.0.0.1:9090/itinerarios/')
+}
+export const getAllRutas=()=>{
+    return axios.get('http://127.0.0.1:9090/rutas/')
+}
